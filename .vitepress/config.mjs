f127df6d199cc98516ccc07248d71c2ce2ec136a
Adam Ben-David <adam.bendavid@gmail.com>
2024-08-16 16:04:53 +0100

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "vitepress"
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
 	title: "Falcon Protocol",
-	 base: '/',
+	 base: '/docs/',
 	description: "Documentation on the Falcon Protocol",
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
